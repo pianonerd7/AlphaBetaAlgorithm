8b090e2a5ddb0075963fe26e6f51e544eff454a2
pianonerd7 <pianonerd7@gmail.com>
2016-02-28 22:18:48 -0500

    updated maplocation

--- src/edu/cwru/sepia/agent/minimax/GameState.java
+++ src/edu/cwru/sepia/agent/minimax/GameState.java
@@ -27,10 +27,6 @@ public class GameState {
 	class MapLocation {
 		public int x;
 		public int y;
-		public int heuristicCost = 0;
-		public int functionCost = 0;
-		public int nodeCost = 0;
-		public MapLocation cameFrom;
 
 		public MapLocation(int x, int y) {
 			this.x = x;
@@ -39,8 +35,7 @@ public class GameState {
 
 		@Override
 		public String toString() {
-			return "x: " + x + ", y: " + y + ", f: " + functionCost + ", heuristic: " + heuristicCost + ", nodecost: "
-					+ nodeCost;
+			return "x: " + x + ", y: " + y;
 		}
 	}
 
